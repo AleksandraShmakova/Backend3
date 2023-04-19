8b4054b497d8189d9c53cd8efb4f5c72ad80ee52
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2023-04-19 10:28:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ $limbs = intval($_POST['radio-2']);
 $superpowers = array($_POST['super']);
 $bio= $_POST['bio'];
 
-$bioreg = "/^[а-яА-ЯёЁa-zA-Z0-9\-_]+$/";
-$reg = "/^[а-яА-ЯёЁa-zA-Z0-9\-_]+$/";
+$bioreg = "/^\s*\w+[а-яА-ЯёЁ\w\s\.,-]*$/";
+$reg = "/^\w+[а-яА-ЯёЁ\w\s-]*$/";
 $mailreg = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
 $list_sup = array('inv','walk','fly');
 
